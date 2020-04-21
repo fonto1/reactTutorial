d8f5b63f04457078c7262776a9f1fd655d3c0d70
fon <FonTo_85@hotmail.com>
2020-04-21 15:39:56 +0200

    first tutorial

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+/*
+export default function Greet() {
+    return (
+        <h1>HEllp sappo sap</h1>
+    )
+}
+
+*/
+
+ export const Greet = () => 
+    <h1>HEllp sappo sap</h1>
+
+ 
